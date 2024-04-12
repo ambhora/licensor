b1c8e5108f64e280328600ea653510d5b577696f
Jayesh Badwaik <j.badwaik@fz-juelich.de>
2024-04-12 06:06:57 +0200

    + initial commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+<!--
+- SPDX-License-Identifier: Apache-2.0
+- Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+-->
+
+# licensor
+
+licensor is tool to manage author and license information in a software project. licensor uses
+[SPDX](https://spdx.org/) identifiers and ensures that the author information is consistent with the
+git history.
+
+
+
+
+
---
+++ src/licensor/metadata.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+import semver
+
+
+__version__ = "0.0.1"
+version = semver.VersionInfo.parse(__version__)
